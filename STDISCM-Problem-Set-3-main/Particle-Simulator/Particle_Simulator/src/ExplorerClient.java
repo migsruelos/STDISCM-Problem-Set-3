85f4c9ec164eb03e2f928a51b20c08f8a3e6984e
DennisAlwyn <104767447+DennisAlwyn@users.noreply.github.com>
2024-04-07 19:08:20 +0800

    Explorer simulator canvas implemented. Made sprite smaller and centered the box

--- STDISCM-Problem-Set-3-main/Particle-Simulator/Particle_Simulator/src/ExplorerClient.java
+++ STDISCM-Problem-Set-3-main/Particle-Simulator/Particle_Simulator/src/ExplorerClient.java
@@ -1,8 +1,16 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class ExplorerClient extends JFrame implements KeyListener {
     private Socket socket;
@@ -13,13 +21,118 @@ public class ExplorerClient extends JFrame implements KeyListener {
     JPanel panel;
     private final int FRAME_WIDTH = 1600;
     private final int FRAME_HEIGHT = 900;
-    private final int WIDTH = 1280;
-    private final int HEIGHT = 720;
     private final int PERIPHERY_WIDTH = 33;
     private final int PERIPHERY_HEIGHT = 19;
 
-    private class ECanvas extends JPanel{
+    //Rendering stuff
+    private List<Particle> particles;
+    private List<Particle> explorers;
+    private BufferedImage spriteImage;
+    private ECanvas canvas;
+    private Particle explorerSprite;
+    private boolean explorerSpawned = true;
+    private final int SPRITE_SIZE = 30;
 
+    private class ECanvas extends JPanel{
+        private final int WIDTH = 1280;
+        private final int HEIGHT = 720;
+
+        public ECanvas(){
+            particles = new ArrayList<>();
+            setPreferredSize(new Dimension(WIDTH, HEIGHT));
+            setFocusable(true);
+            requestFocusInWindow();
+
+            try {
+                spriteImage = ImageIO.read(new File("STDISCM-Problem-Set-3-main/Particle-Simulator/Particle_Simulator/src/sprite/sprite.png"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
+            executorService.scheduleAtFixedRate(this::calculateFPS, 0, 500, TimeUnit.MILLISECONDS);
+        }
+
+        private void renderExplorerMode(Graphics g) {
+            g.setColor(Color.GREEN);
+            for (Particle particle : particles) {
+                int distanceX = (int) (particle.x - explorerSprite.x);
+                int distanceY = (int) (particle.y - explorerSprite.y);
+                if(Math.abs(distanceX) > PERIPHERY_WIDTH || Math.abs(distanceY) > PERIPHERY_HEIGHT)
+                    continue;
+
+                g.fillOval((int) distanceX * (WIDTH/PERIPHERY_WIDTH),
+                        (int) distanceY * (HEIGHT/PERIPHERY_HEIGHT), 10, 10);
+            }
+
+            if (explorerSpawned && spriteImage != null) {
+                g.drawImage(spriteImage, WIDTH / 2 - SPRITE_SIZE * 4, HEIGHT / 2 - SPRITE_SIZE * 4,
+                        SPRITE_SIZE*8, SPRITE_SIZE*8, null);
+            }
+        }
+
+        private void updateFPS() {
+            fps = frameCount;
+            frameCount = 0;
+            lastFPSTime = System.currentTimeMillis();
+
+            setTitle("Particle Simulator | FPS: " + calculateFPS() + " | X: "
+                    + explorerSprite.x + " Y: " + explorerSprite.y);
+        }
+
+        private int calculateFPS() {
+            long currentTime = System.currentTimeMillis();
+            long elapsedTime = currentTime - lastFPSTime;
+
+            frameCount++;
+
+            if (elapsedTime >= 1000) {
+                updateFPS();
+            }
+
+            return fps;
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+
+            Image offscreen = createImage(getWidth(), getHeight());
+            Graphics2D offscreenGraphics = (Graphics2D) offscreen.getGraphics();
+            offscreenGraphics.setColor(Color.BLACK);
+            offscreenGraphics.fillRect(0,0, WIDTH, HEIGHT);
+
+            renderExplorerMode(offscreenGraphics);
+
+            calculateFPS();
+
+            g.drawImage(offscreen, 0, 0, this);
+        }
+
+        void update() {
+            calculateFPS();
+            double deltaTime = 0.05;
+
+            // minimum target FPS: 60
+            long targetFrameTime = 1000 / 60;
+            long currentTime = System.currentTimeMillis();
+
+            // submit rendering tasks for particles
+            //TODO: Get particle coords from server
+            repaint();
+
+            // calculates the time taken for the update and rendering tasks
+            long elapsedTime = System.currentTimeMillis() - currentTime;
+
+            // sleep to maintain a consistent frame rate
+            long sleepTime = Math.max(0, targetFrameTime - elapsedTime);
+
+            try {
+                Thread.sleep(sleepTime);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     //FPS stuff
@@ -44,7 +157,11 @@ public class ExplorerClient extends JFrame implements KeyListener {
         setTitle("Explorer Client | FPS: 0");
         panel = new JPanel();
         panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
-        panel.setBorder(BorderFactory.createEmptyBorder(40, 20, 0, 0));
+        panel.setBorder(BorderFactory.createEmptyBorder(40, 120, 0, 0));
+        canvas = new ECanvas();
+        panel.add(canvas);
+        setFocusable(true);
+        requestFocusInWindow();
 
         add(panel);
         setSize(FRAME_WIDTH, FRAME_HEIGHT);
@@ -52,6 +169,14 @@ public class ExplorerClient extends JFrame implements KeyListener {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setExtendedState(java.awt.Frame.MAXIMIZED_BOTH);
         setVisible(true);
+
+        //Load Explorer client particle TODO: Maybe add a promt for coords on where to spawn?
+        explorerSprite = new Particle(500, 300, 0, 0);
+
+        Timer timer = new Timer(15, e -> {
+            canvas.update();
+        });
+        timer.start();
     }
 
     public void start() {
